77984250fa0e9c96c90420aa5bb783721323da2a
TanhBb <anhntgcc200302@fpt.edu.vn>
2022-10-20 18:24:57 +0700

    connect pg

--- connection.php
+++ connection.php
@@ -1,4 +1,7 @@
-<?php 
-    $conn = mysqli_connect('localhost', 'root', '', 'bb_store')
-            or die("Can not database".mysqli_connect_error());
+<?php
+	$conn = pg_connect("postgres://hnrrtrikmoyman:9d112ca9a3b6d06e4fb062eb656fa25fe480d02e34aec9238654d371e434cabf@ec2-54-147-36-107.compute-1.amazonaws.com:5432/d471sfk7dsih5a");
+	
+    if (!$conn) {
+        die("Connection failed");
+    }
 ?>
